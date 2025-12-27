2c8bf2ad993e34a9eb7a45cd2d9913dbca7e6a4b
yassin549 <khoualdiyassin26@gmail.com>
2025-12-27 05:43:51 +0100

    commit

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,7 +5,17 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const nextConfig = {
     transpilePackages: ['@megatron/database', '@megatron/lib-common', '@megatron/lib-crypto'],
     images: {
-        domains: ['avatars.githubusercontent.com'],
+        domains: [
+            'avatars.githubusercontent.com',
+            'images.unsplash.com',
+            'plus.unsplash.com',
+            'cryptologos.cc',
+            'assets.coingecko.com',
+            'cdn.jsdelivr.net',
+            'raw.githubusercontent.com',
+            'upload.wikimedia.org',
+            'wikipedia.org'
+        ],
     },
 };
 
