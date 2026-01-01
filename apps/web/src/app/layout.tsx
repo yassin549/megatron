f42a22f319dea424772ee452d8fa0541f46242c6
yassin549 <khoualdiyassin26@gmail.com>
2026-01-01 05:38:37 +0100

    comit

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
                         </div>
                         <MobileBottomNav />
                         <RequestMarketButton />
+                        <InitialLoader />
                     </div>
                     <Analytics />
                     <SpeedInsights />
