8ea577e0a3588411fb04ae9e506ccb7c53adb0c5
yassin549 <khoualdiyassin26@gmail.com>
2026-01-18 10:41:16 +0100

    commit

--- apps/web/src/app/assets/[id]/AssetDetailClient.tsx
+++ apps/web/src/app/assets/[id]/AssetDetailClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { createPortal } from 'react-dom';
 import Link from 'next/link';
 import { ArrowLeft, X, Plus } from 'lucide-react';
