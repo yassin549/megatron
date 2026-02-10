b0ec226287c569f733cf5061265ea9d58f5e61d6
yassin549 <khoualdiyassin26@gmail.com>
2026-02-10 13:34:15 +0100

    commit

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -19,6 +19,22 @@ const nextConfig = {
             's2.coinmarketcap.com'
         ],
     },
+    webpack: (config) => {
+        config.externals.push({
+            'utf-8-validate': 'commonjs utf-8-validate',
+            'bufferutil': 'commonjs bufferutil',
+            'onnxruntime-node': 'commonjs onnxruntime-node',
+        });
+        // Ignore .node files to prevent webpack loader errors
+        config.module.rules.push({
+            test: /\.node$/,
+            use: 'node-loader',
+        });
+        return config;
+    },
+    experimental: {
+        serverComponentsExternalPackages: ['onnxruntime-node', '@huggingface/transformers', 'sharp'],
+    },
 };
 
 module.exports = nextConfig;
