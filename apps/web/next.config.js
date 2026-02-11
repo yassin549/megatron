4907ae8e348aa071f736b3a467e3ad37664b5ce3
yassin549 <khoualdiyassin26@gmail.com>
2026-02-11 16:50:19 +0100

    commit

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -29,15 +29,14 @@ const nextConfig = {
     },
     experimental: {
         serverComponentsExternalPackages: ['sharp'],
+        outputFileTracingRoot: path.join(__dirname, '../../'),
         outputFileTracingExcludes: {
             '*': [
-                'node_modules/@swc/core-linux-x64-gnu',
-                'node_modules/@swc/core-linux-x64-musl',
-                'node_modules/@esbuild/linux-x64',
-                'node_modules/webpack',
-                'node_modules/terser',
-                'node_modules/.pnpm',
-                '../../node_modules/**'
+                '**/node_modules/@swc/core-*',
+                '**/node_modules/@esbuild/*',
+                '**/node_modules/webpack/**/*',
+                '**/node_modules/terser/**/*',
+                '**/.cache/**/*'
             ],
         },
         outputFileTracingIncludes: {
